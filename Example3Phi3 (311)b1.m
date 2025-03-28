44976c57f1222cfb1aec6cb8a9bb53b1bc17489d
Ram Karan Choudhary <ramkchoudhary1997@gmail.com>
2025-03-28 11:38:48 +0530

    Add files via upload

---
+++ Example3Phi3 (311)b1.m
@@ -0,0 +1,56 @@
+// Phi3 (311)b1
+ p := 3;
+ F<a, a1, a2, a3> := FreeGroup(4);
+
+ // Define the relations explicitly given in the presentation,
+ // including the trivial commutators that are omitted in a minimal presentation.
+ Comms := [
+     (a1, a) = a2,
+     (a2, a) = a3,
+     (a3, a) = 1,
+     a1^(p^2) = a3,
+     a^p = 1,
+     a2^p = 1,
+     a3^p = 1,
+    (a2, a1)=1,
+    (a3, a1)=1,
+    (a3, a2)=1
+    ];
+
+ // Define the quotient group from the presentation
+ Q := quo< F | Comms >;
+
+ // Compute the p-quotient of Q to a given class (here, 5)
+ G, phi := pQuotient(Q, p, 5);
+
+ // Define H
+ D := DerivedGroup(G);
+ H := Centraliser(G, D);
+
+ S := [ H];
+
+
+ G, delta := MyPermRep(G);
+
+subs := [delta (H) : H in S];
+IdentifyGroup(G);
+time M := AllInequivalentRepresentations(G, subs);
+M;
+
+CG := CharacterTable(G); // Stores the character table without displaying it
+#CG; // Shows only the number of characters, not the full table
+
+for i in [1..#CG] do
+char := CG[i];
+
+if Degree (char) eq 1 then continue;
+end if;
+
+for H in subs do
+IsRequiredPair(G, H, char);
+end for;
+end for;
+
+ConstructRep(G, CG[5]);
+
+RationalRepresentation(G, subs[1], CG[45]);
